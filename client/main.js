52f1d0065665d32639aca5276763875b061cd426
n1c01a5 <contact@wagner-nicolas.com>
2016-10-31 16:53:21 +0100

    add web3js with metamask

--- client/main.js
+++ client/main.js
@@ -1,4 +1,3 @@
-
 import { render } from 'react-dom'
 import React from 'react'
 import { Meteor } from 'meteor/meteor'
@@ -6,3 +5,8 @@ import { Meteor } from 'meteor/meteor'
 import App from './imports/ui/App'
 
 Meteor.startup(() => render(<App />, document.getElementById('main')))
+
+web3.eth.getBalance("0x73Aee431736394DB61aA172B95b954D60435b761",
+function (err, res) {
+  alert(res);
+})
