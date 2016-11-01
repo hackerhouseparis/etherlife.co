fbea08bf1367186a0dbfd6396dd31b2e4c0757ca
n1c01a5 <contact@wagner-nicolas.com>
2016-11-01 13:22:06 +0100

    change style of button

--- client/imports/ui/Button.js
+++ client/imports/ui/Button.js
@@ -9,7 +9,7 @@ export default ({
   icon = 'rocket',
   action = v => v,
 }) => (
-  <p className='Button' onClick={action}>
+  <p className='Button' onClick={action} style={{fontSize: '30px', padding: '10px 15px 10px 15px', margin: '100px 0 0 0'}}>
     <i className={`fa fa-${icon}`} />
     <span>{content}</span>
   </p>
