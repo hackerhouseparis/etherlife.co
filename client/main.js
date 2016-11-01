c21556b673bef5ed71222a7e02eb9e59b3c75172
n1c01a5 <contact@wagner-nicolas.com>
2016-11-01 14:23:16 +0100

    get the public key

--- client/main.js
+++ client/main.js
@@ -6,7 +6,10 @@ import App from './imports/ui/App'
 
 Meteor.startup(() => render(<App />, document.getElementById('main')))
 
-web3.eth.getBalance("0x73Aee431736394DB61aA172B95b954D60435b761",
+
+console.log(web3.eth.defaultAccount)
+
+web3.eth.getBalance(web3.eth.defaultAccount,
 function (err, res) {
-  alert(res);
+  console.log(res);
 })
